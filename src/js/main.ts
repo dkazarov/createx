96045593bd1c339ac1aa9278e49bc43197bd89ac
unknown <d.kazarov1361@gmail.com>
2024-12-24 21:00:38 +0200

    footer: go to top btn add ankor link to hero section

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,4 +1,8 @@
 import { burgerOpen, burgerClose } from './js-components/burger';
 
+const goTopBtn = document.querySelector('#go-to-top');
+
+console.log(goTopBtn);
+
 burgerOpen();
 burgerClose();
