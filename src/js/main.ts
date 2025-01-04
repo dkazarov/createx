c438a21bae4ae973c29d61303d0fc41ad05ad29d
unknown <d.kazarov1361@gmail.com>
2025-01-04 15:38:00 +0200

    hero: add base markup

--- src/js/main.ts
+++ src/js/main.ts
@@ -4,13 +4,16 @@ import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 
 const swiper = new Swiper('.hero-slider', {
-	modules: [Navigation, Pagination, ],
+	modules: [Navigation, Pagination],
 
-  loop: true,
+	loop: true,
 
 	pagination: {
 		el: '.swiper-pagination',
-    clickable: true,
+		clickable: true,
+		renderBullet: function (index, className) {
+			return '<span class="' + className + '">' + (index + 1) + '</span>';
+		},
 	},
 
 	// Navigation arrows
