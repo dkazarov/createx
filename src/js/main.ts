77d200a606e0a51da7c87ec798fbeefd0a436198
unknown <d.kazarov1361@gmail.com>
2025-01-04 15:21:36 +0200

    hero: connect swiper

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,4 +1,24 @@
 import { burgerOpen, burgerClose } from './js-components/burger';
 
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper('.hero-slider', {
+	modules: [Navigation, Pagination, ],
+
+  loop: true,
+
+	pagination: {
+		el: '.swiper-pagination',
+    clickable: true,
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+
 burgerOpen();
 burgerClose();
